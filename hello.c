e99a10b4b59b64ae6001e772baeaf21a5ab7936e
knogu <kotaronoguchi61@gmail.com>
2023-12-29 04:13:43 +0000

    Get int arg value in hook

--- hello.c
+++ hello.c
@@ -1,10 +1,34 @@
+#include "test.h"
 #include <stdio.h>
 
-void callee() {
-    printf("Hello from callee\n");
+void dump(void* map, char *name) {
+    size_t len = get_arg_count(map, name);
+
+    for (int i = 0; i < len; ++i) {
+        Arg arg = get_ith_arg(map, name, i);
+        if (arg.is_arg) {
+            printf("=== %d th arg ===\n", i+1);
+        } else {
+            printf("=== %d th member ===\n", i+1);
+        }
+
+        printf("name: %s\n", arg.name);
+        printf("bytes_cnt: %llu\n", arg.bytes_cnt);
+        printf("location: %lld\n", arg.location);
+        printf("type_name: %s\n", arg.type_name);
+        printf("\n");
+    }
 }
 
 int main() {
-    callee();
-    printf("hello\n");
+    // Rustのcreate_map関数を呼び出す
+    void* map = get_func2args("/Users/jp31281/call-tracer/dinfo/samples/hello");
+
+    dump(map, "callee");
+    dump(map, "Person");
+
+    // Rustのfree_map関数を呼び出す
+//    free_map(map);
+
+    return 0;
 }
